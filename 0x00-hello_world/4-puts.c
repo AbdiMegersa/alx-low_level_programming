ddea0d7c3def57c6320b43e7c70618b142672b67
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-26 20:02:11 +0300

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+
 /*
  * this description for the main funcion 
  * and you have to deal with it okay 
  * you checker
  */
 
-
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
