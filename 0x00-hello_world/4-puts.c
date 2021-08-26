26001bb876d43c5f1d8d7f4fd2fc892b1f2d1dad
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-26 20:03:58 +0300

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
- * this description for the main funcion 
- * and you have to deal with it okay 
- * you checker
+/**
+ * main - Entry point 
+ * 
+ * Return: Always 0 (Success)
  */
 
 int main(void)
