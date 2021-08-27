f80c0447bfe98b915a879dcb44d0ff2832584d6d
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-27 17:17:13 +0300

    no e/q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,10 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a'; (i != 'e' && i != 'q' && i <= 'z'); i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
+		if (i == 'e' || i == 'q')
+			continue;
 		putchar(i);
 	}
 	putchar('\n');
