2d649eda2e727fd4182c93ffdb3c4a1a2c106bf8
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-27 17:25:50 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: random number in n +ve/-ve
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
