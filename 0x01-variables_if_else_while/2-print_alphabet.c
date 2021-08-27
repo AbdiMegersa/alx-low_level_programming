a51d306a8e57e17781c47093e72a8f98675bfc13
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-27 16:43:22 +0300

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
- * this comment thing is
- * getting on my nerves
- * i mean does they read this sht
+/**
  * main - entry point
+ *
+ * return 0 if success
  */
 int main(void)
 {
