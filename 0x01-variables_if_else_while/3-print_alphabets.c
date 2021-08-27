031bea2697f00a49ca83481964547e7c5086d6d4
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-27 16:51:02 +0300

    both cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <>
+#include <>
+
+/*
+ * main - entry point
+ *
+ * return: 0 (Succcess)
+ */
+int main(void)
+{
+	char low;
+	chat up;
+	for (char low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (char up = 'A'; up <= 'Z'; up++)
+	{
+		putcahr(up)
+	}
+	putchar('\n');
+	return (0);
+}
