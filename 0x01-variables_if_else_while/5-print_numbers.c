c3acd62c080975e8326f1e30729002017e23fa14
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-27 17:21:18 +0300

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Descriptioin: print numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
