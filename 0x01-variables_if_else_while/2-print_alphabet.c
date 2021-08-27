4441097efa2c4f00335aea6c64fbd48e7149e8a1
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-27 13:51:09 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* 
+ * this comment thing is 
+ * getting on my nerves
+ * i mean does they read this sht
+ * i hate you guys
+ */
+int main(void)
+{
+	for(char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar("\n");
+
+	}
+	return (0);
+}
