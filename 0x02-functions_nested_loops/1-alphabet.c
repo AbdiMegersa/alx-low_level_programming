b42426c984ab2ac3e925767813fe4942d30cd057
AbdiMegersa <abdimegersaerega@gmail.com>
2021-08-31 22:09:26 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/*
+ * main -Entry point
+ *  
+ * description : print alphabets
+ *
+ * return: (0) if success
+ */
+int main(vo`:xid)
+{
+	print_alphabet();
+	return (0);
+}
+
