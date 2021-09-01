12d559c74056a31e55727ac6988cdd7898a1a0a4
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-01 19:33:41 +0300

    error gone

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,2 +1,27 @@
-#include <main.h>
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+void positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n < 0)
+		printf("%d is negative", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	printf("\n");
+}
+
+int main(void)
+{
+        positive_or_negative(i);
+        return (0);
+}
 
