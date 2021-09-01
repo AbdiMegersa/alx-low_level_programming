31fa3b8d6d2857683cb1bf74368d027e4f7cf43b
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-01 20:04:18 +0300

    largest

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,23 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c)
+{
+    int largest;
 
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else if (c > a && c > b)
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
 #endif /* MAIN_H */
