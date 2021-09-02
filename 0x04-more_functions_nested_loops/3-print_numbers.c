95aa89f2ddfd7defa5cd641f921f22f271ef0168
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-02 21:46:58 +0300

    print nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 - 9
+ *
+ * return: 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
