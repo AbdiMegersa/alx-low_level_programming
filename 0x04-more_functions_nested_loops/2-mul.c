8efb3d517925dd795a7a97936b4aff5f0ee95461
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-02 21:38:24 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for a digit.
+ *@c: input.
+ *
+ *Return: 1 if c is a digit
+ *0 otherwise.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	putchar(c);
+	return (0);
+}
