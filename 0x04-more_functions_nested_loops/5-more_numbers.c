b2cd73b3a09836c7eb4e0685bb8f596738c8334a
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-02 22:03:43 +0300

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+
+}
+
+
