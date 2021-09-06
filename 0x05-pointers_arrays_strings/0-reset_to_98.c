f19ee6bd176931a3fe9001bbe5dd718eab7a8f4e
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-06 20:04:42 +0300

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
- *reset_to_98 - converts the value of the passed pointer to 98
- *@n - is the pointer(address) passed
+ *reset_to_98 - converts the value of the passed.
+ *@n - pointer.
  *
  *Return: void
  */
