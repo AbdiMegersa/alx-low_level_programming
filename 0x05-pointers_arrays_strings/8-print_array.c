3028a33e08723951eab004e045a8a3c6e80a9deb
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-06 22:49:05 +0300

    updated

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - print arrays
+ * @a: first member
+ * @n: second member
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
