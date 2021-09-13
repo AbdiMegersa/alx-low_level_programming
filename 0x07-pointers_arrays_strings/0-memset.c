20ccca24f3b18cbd66d01fa082496bda44f32941
AbdiMegersa <abdimegersaerega@gmail.com>
2021-09-13 12:13:44 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "mairen.h"
+/**
+ * _memset -y  fulls the first n bytes of memory area pointed to.
+ * @s: first member
+ * @b: second memeber
+ * @n: third member
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	s = &b;
+	
