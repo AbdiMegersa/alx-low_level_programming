46c510bb0fc6193b843ba046ca38c7ac079de4b9
AbdiMegersa <88324719+AbdiMegersa@users.noreply.github.com>
2021-10-22 10:17:11 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,9 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int opener;
+	int opener, Read, Write;
 	char *reader;
-	int Read;
-	int Write;
-
+	
 	if (filename == NULL)
 		return (0);
 	opener = open(filename, O_RDONLY);
@@ -33,5 +31,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	close(opener);
 	free(reader);
+	
 	return (Write);
 }
